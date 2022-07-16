aa0e8269c8870d6b1174aa14d5f1195ce53b5f5b
als698 <mb@als>
2022-07-16 15:50:11 +0300

    rubik

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'fe';
+  title = 'Rubik';
+  cube: any = [];
+
+  constructor(private api: ApiService) {
+    this.getCube();
+  }
+
+  getCube() {
+    this.api.call('getCube').subscribe({
+      next: (data: any) => {
+        this.cube = data;
+      },
+    });
+  }
 }
