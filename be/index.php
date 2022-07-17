7bbfb8bddeb561b8db0844c0570caddb39237e42
als698 <mb@als>
2022-07-17 19:28:03 +0300

    index, session, request

--- be/index.php
+++ be/index.php
@@ -2,10 +2,40 @@
 require_once('app/config.php');
 require_once('app/main.php');
 
-$app = new Main();
+class Index {
+    public $request = [];
+
+    public function __construct() {
+        $this->request = json_decode(file_get_contents("php://input"));
+        if (isset($this->request->session)) {
+            session_id($this->request->session);
+            session_start();
+        } else {
+            session_start();
+        }
+    }
+}
 
 if (isset($_GET['r'])) {
-    if ($_GET['r'] == 'getCube') {
-        $app->gCube();
+    $app = new Main();
+
+    switch ($_GET['r']) {
+        case 'getSess':
+            echo json_encode(session_id(), 1);
+            break;
+        case 'getCube':
+            $app->gCube();
+            break;
+        case 'moveCube':
+            $app->mCube();
+            break;
+        case 'randCube':
+            $app->randCube();
+            break;
+        case 'resetCube':
+            $app->sCube();
+            break;
+        default:
+            exit(0);
     }
 }
